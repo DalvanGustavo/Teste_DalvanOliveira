5e9915bb0f6daa9e657b0556ee33f8641f0eb325
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-02-01 02:59:25 -0300

    Verificar se um arquivo contém a expressão.

--- processamento_dados.py
+++ processamento_dados.py
@@ -9,3 +9,22 @@ extensoes_suportadas = (".csv", ".txt", ".xlsx")
 def contem_expressao_alvo(df):
     df_str = df.astype(str).apply(lambda col: col.str.lower())
     return df_str.apply(lambda col: col.str.contains(expressao_alvo, regex=False, na=False)).any().any()
+
+# Função para verificar se um arquivo contém a expressão alvo
+def arquivo_contem_sinistros(caminho):
+
+    # Tentar ler o arquivo com base na extensão
+    try:
+        if caminho.lower().endswith(".csv"):
+            df = pd.read_csv(caminho, nrows=1000, sep=None, engine="python")
+        elif caminho.lower().endswith(".txt"):
+            df = pd.read_csv(caminho, nrows=1000, sep=";", engine="python")
+        elif caminho.lower().endswith(".xlsx"):
+            df = pd.read_excel(caminho, nrows=1000)
+        else:
+            return False
+
+        return contem_expressao_alvo(df)
+
+    except Exception:
+        return False

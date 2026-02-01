301a7f749341b28634db8f1de6f194e68eec0b96
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-02-01 13:48:08 -0300

    Validação de CNPJs, com verificação dos dígitos, despresando caracteres não numéricos, com cálculo dos dígitos verificadores.

--- validacao_dados.py
+++ validacao_dados.py
@@ -21,3 +21,27 @@ def extrair_zip():
             f"{csv_entrada} não encontrado após extração."
         )
 
+# Função para validar CNPJ
+def validar_cnpj(cnpj):
+
+    # Remover caracteres não numéricos
+    cnpj = re.sub(r"\D", "", str(cnpj))
+
+    # Verificar tamanho e sequências inválidas
+    if len(cnpj) != 14 or cnpj == cnpj[0] * 14:
+        return False
+
+    # Cálculo dos dígitos verificadores
+    def calcular_digito(cnpj_parcial, pesos):
+        soma = sum(int(cnpj_parcial[i]) * pesos[i] for i in range(len(pesos)))
+        resto = soma % 11
+        return "0" if resto < 2 else str(11 - resto)
+
+    pesos_1 = [5,4,3,2,9,8,7,6,5,4,3,2]
+    pesos_2 = [6] + pesos_1
+
+    # Cálculo dos dígitos verificadores
+    dig1 = calcular_digito(cnpj[:12], pesos_1)
+    dig2 = calcular_digito(cnpj[:13], pesos_2)
+
+    return cnpj[-2:] == dig1 + dig2

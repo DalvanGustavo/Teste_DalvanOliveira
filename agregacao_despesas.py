846a0de97f47b96b2d61bb1f5ba97c5745182b7f
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-02-01 22:33:01 -0300

    Compactando arquivo para ZIP.

--- agregacao_despesas.py
+++ agregacao_despesas.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import zipfile
-
+import os
 # Definição dos nomes dos arquivos
 zip_entrada = "consolidado_despesas_enriquecido.zip"
 csv_entrada = "consolidado_despesas_enriquecido.csv"
 csv_saida = "despesas_agregadas.csv"
+zip_final = "Teste_DalvanOliveira.zip"
 
 # Função para extrair o arquivo ZIP
 def extrair_zip():
@@ -37,11 +38,20 @@ def agregar_dados():
 
     return agrupado
 
+def gerar_zip_final(df):
+    df.to_csv(csv_saida, index=False)
+
+    with zipfile.ZipFile(zip_final, "w", zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(csv_saida)
+
+    os.remove(csv_saida)
+
+
 # Execução do script
 if __name__ == "__main__":
     extrair_zip()
     df_agregado = agregar_dados()
-    df_agregado.to_csv(csv_saida, index=False)
+    gerar_zip_final(df_agregado)
 
     print("Agregação concluída")
     print(f"Arquivo gerado: {csv_saida}")
